5caf0627bb1d96b16a4316768a3f688ac6612361
Brenton Poke <brentonpoke@outlook.com>
2020-01-17 09:04:57 -0500

    just getting rid of annotations

--- src/main/java/gov/noaa/points/PointData.java
+++ src/main/java/gov/noaa/points/PointData.java
@@ -1,14 +1,12 @@
 package gov.noaa.points;
 
 import com.github.filosganga.geogson.model.Geometry;
-import javax.annotation.Generated;
 import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@Generated("com.robohorse.robopojogenerator")
 public class PointData{
 
 	@SerializedName("geometry")
--- src/main/java/gov/noaa/points/PointProperties.java
+++ src/main/java/gov/noaa/points/PointProperties.java
@@ -1,13 +1,11 @@
 package gov.noaa.points;
 
-import javax.annotation.Generated;
 import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@Generated("com.robohorse.robopojogenerator")
 public class PointProperties{
 
 	@SerializedName("radarStation")
